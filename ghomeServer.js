ea51b772e1f56ae08e2869d8c78df4617ac3265b
Kuri <kli@yahoo-corp.jp>
2018-04-18 13:28:07 +0900

    for presentation

--- ghomeServer.js
+++ ghomeServer.js
@@ -31,18 +31,10 @@ app.post('/google-home-notifier', urlencodedParser, function (req, res) {
 
   if (text){
     try {
-      if (text.startsWith('http')){
-        var mp3_url = text;
-        googlehome.play(mp3_url, function(notifyRes) {
-          console.log(notifyRes);
-          res.send(deviceName + ' will play sound from url: ' + mp3_url + '\n');
-        });
-      } else {
-        googlehome.notify(text, function(notifyRes) {
-          console.log(notifyRes);
-          res.send(deviceName + ' will say: ' + text + '\n');
-        });
-      }
+      googlehome.notify(text, function(notifyRes) {
+        console.log(notifyRes);
+        res.send(deviceName + ' will say: ' + text + '\n');
+      });
     } catch(err) {
       console.log(err);
       res.sendStatus(500);
@@ -53,50 +45,10 @@ app.post('/google-home-notifier', urlencodedParser, function (req, res) {
   }
 })
 
-app.post('/ps4-waker', urlencodedParser, function (req, res) {
-
-  if (!req.body) return res.sendStatus(400)
-  console.log(req.body);
-
-  var text = req.body.text;
-
-  if (text){
-    try {
-      if (text.startsWith('start')) {
-        var gameName = text.slice(6);
-        if (gameName in settings.games) {
-          res.send(gameName + ' will be started');
-          var exec = require('child_process').exec;
-          exec('sudo ps4-waker start ' + settings.games[gameName], function(err, stdout, stderr){
-            console.log(stdout);
-          });
-        } else {
-          res.send('You need to buy ' + gameName + ' first!');
-        }
-      } else if (text === 'game off') {
-        var exec = require('child_process').exec;
-        exec('sudo ps4-waker standby', function(err, stdout, stderr){
-          res.send('Your ps4 will go to sleep');
-          console.log(stdout);
-        });
-      } else {
-        res.sendStatus(400);
-      }
-    } catch(err) {
-      console.log(err);
-      res.sendStatus(500);
-      res.send(err);
-    }
-  }else{
-    res.send('Please POST "text=start monster hunter"');
-  }
-})
-
 app.listen(serverPort, function () {
   ngrok.connect(serverPort, function (err, url) {
     console.log('Endpoints:');
     console.log('  google home notifier: ' + url + '/google-home-notifier');
-    console.log('  ps4 waker: ' + url + '/ps4-waker');
     console.log('POST example:');
     console.log('curl -X POST -d "text=Hello Google Home" ' + url + '/google-home-notifier');
   });
--- ghomeSettings.js
+++ ghomeSettings.js
@@ -10,12 +10,3 @@ exports.ghomeName = 'MyHome';
 // your google home's ip address
 // can be found from your google home app
 exports.ghomeIp = 'xxx.xxx.xxx.xxx';
-
-// The games in your PS4
-// On the left is any text your want for a game
-// On the right is the title id for the game
-// The title ids and be found in the url of the game on sony store
-exports.games = {
-  'monster hunter': 'CUSA06027',
-  'bloodborne': 'CUSA00299'
-};
